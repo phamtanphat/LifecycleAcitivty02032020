811eccbe8a6c481a9abeeae2fd4e2799c9a6930d
phatdroid94 <phatdroid94@gmail.com>
2020-07-18 18:04:54 +0700

    create main 2

--- app/src/main/java/com/example/lifecycleacitivty02032020/MainActivity.java
+++ app/src/main/java/com/example/lifecycleacitivty02032020/MainActivity.java
@@ -15,13 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("BBB","onCreate");
+        Log.d("BBB","onCreate Main1");
         mBtnNavigate = findViewById(R.id.buttonNavigateScreen2);
 
         mBtnNavigate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
             }
         });
     }
@@ -29,36 +30,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("BBB","onStart");
+        Log.d("BBB","onStart Main1");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("BBB","onResume");
+        Log.d("BBB","onResume Main1");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d("BBB","onPause");
+        Log.d("BBB","onPause Main1");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d("BBB","onStop");
+        Log.d("BBB","onStop Main1");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d("BBB","onRestart");
+        Log.d("BBB","onRestart Main1");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d("BBB","onDestroy");
+        Log.d("BBB","onDestroy Main1");
     }
 }
--- app/src/main/java/com/example/lifecycleacitivty02032020/MainActivity2.java
+++ app/src/main/java/com/example/lifecycleacitivty02032020/MainActivity2.java
@@ -3,12 +3,52 @@ package com.example.lifecycleacitivty02032020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    Button mBtnNavigate3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Log.d("BBB","onCreate Main2");
+        mBtnNavigate3 = findViewById(R.id.buttonNavigateScreen3);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("BBB","onStart Main2");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("BBB","onResume Main2");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("BBB","onPause Main2");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("BBB","onStop Main2");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("BBB","onRestart Main2");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("BBB","onDestroy Main2");
     }
 }
